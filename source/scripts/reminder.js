e9bb34b802013fd2782f00c6e0522c5b7615b162
DavidWangwjh <davidwang.wjh@gmail.com>
2022-11-20 20:45:09 -0800

    fix js styling

--- source/scripts/reminder.js
+++ source/scripts/reminder.js
@@ -1,18 +1,16 @@
 function addReminder() {
-    var reminderListHTML= document.getElementById("reminderList");
-    var reminderLen = reminderListHTML.childNodes.length;
-    var newReminder = document.getElementById("reminder").value;
+    let reminderListHTML= document.getElementById("reminderList");
+    let reminderLen = reminderListHTML.childNodes.length;
+    let newReminder = document.getElementById("reminder").value;
     if (newReminder == ""){
       alert("reminder name cannot be empty")
     } else {
-
       reminderListHTML.innerHTML += `<li id=\"${reminderLen+1}\">&emsp;${newReminder}<button class="remove" onclick="removeReminder(${reminderLen+1});">&minus;</button></li>`;
       document.getElementById("reminder").value = "";
-      console.log(reminderLen);
     }
 }
 
-function removeReminder(elem) {
-  var a = document.getElementById(elem.toString(10))
-  a.remove();
+function removeReminder(reminderId) {
+  let reminder = document.getElementById(reminderId.toString(10))
+  reminder.remove();
 }
