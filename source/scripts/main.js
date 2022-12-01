f22946d29c7d460aa827c09aadd446dafb31cdb0
Alessia <aawelch@ucsd.edu>
2022-12-01 12:23:56 -0800

    fix layout based on toggle

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,19 +1,25 @@
 /*
- * Hide the Reminders and Class on click of toggle button
+ * Hide the Reminders and Class on click of toggle button and fix the containers
  * @param - none
  */
 function hideShowButton() {
-    //get the class to disappeear
-    var container = document.getElementsByClassName("reminders-calendar-container");
+  //get the class to disappeear
+  var container = document.getElementsByClassName("reminders-calendar-container");
+  //get the grid to work
+  var grid = document.querySelector("div.main-body");
     
-    //getElementsByClassName returns an array so for every single one display if
-    //set to none or vice versa
-    Array.from(container).forEach((x) => {
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-          toggleButton = "hide reminder";
-        }
-    })
+  //getElementsByClassName returns an array so for every single one display if
+  //set to none or vice versa
+  Array.from(container).forEach((x) => {
+    if (x.style.display === "none") {
+      grid.classList.remove('toggleOFF');
+      grid.classList.toggle('toggleON');
+      x.style.display = "block"; 
+    } 
+    else {
+      grid.classList.remove('toggleON');
+      grid.classList.toggle('toggleOFF');
+      x.style.display = "none";
+    }
+  })
 }
